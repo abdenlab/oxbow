df5cbc66db44ead0ae317e1dd3f7e53c3a69bb3b
Trevor Manz <trevor.j.manz@gmail.com>
2023-05-26 15:48:20 -0400

    feat: Add R implementation

--- r-oxbow/src/rust/src/lib.rs
+++ r-oxbow/src/rust/src/lib.rs
@@ -1,5 +1,6 @@
 use extendr_api::prelude::*;
 use oxbow::bam::BamReader;
+use oxbow::vcf::VcfReader;
 
 /// Return Arrow IPC format from a BAM file.
 /// @export
@@ -9,10 +10,19 @@ fn read_bam(path: &str, region: Option<&str>) -> Vec<u8> {
     reader.records_to_ipc(region).unwrap()
 }
 
+/// Return Arrow IPC format from a VCF file.
+/// @export
+#[extendr]
+fn read_vcf(path: &str, region: Option<&str>) -> Vec<u8> {
+    let mut reader = VcfReader::new(path).unwrap();
+    reader.records_to_ipc(region).unwrap()
+}
+
 // Macro to generate exports.
 // This ensures exported functions are registered with R.
 // See corresponding C code in `entrypoint.c`.
 extendr_module! {
     mod oxbow;
     fn read_bam;
+    fn read_vcf;
 }
