f4e436cbefe702a7c132f79f74cd329c72a20cdb
Nezar Abdennur <nabdennur@gmail.com>
2023-06-25 23:20:25 -0400

    feat: Initial fasta and fastq readers

--- py-oxbow/src/lib.rs
+++ py-oxbow/src/lib.rs
@@ -1,9 +1,25 @@
 use pyo3::prelude::*;
 use pyo3::types::PyBytes;
 
+use oxbow::fasta::FastaReader;
+use oxbow::fastq::FastqReader;
 use oxbow::bam::BamReader;
 use oxbow::vcf::VcfReader;
 use oxbow::bcf::BcfReader;
+
+#[pyfunction]
+fn read_fasta(path: &str, region: Option<&str>) -> PyObject {
+    let mut reader = FastaReader::new(path).unwrap();
+    let ipc = reader.records_to_ipc(region).unwrap();
+    Python::with_gil(|py| PyBytes::new(py, &ipc).into())
+}
+
+#[pyfunction]
+fn read_fastq(path: &str) -> PyObject {
+    let mut reader = FastqReader::new(path).unwrap();
+    let ipc = reader.records_to_ipc().unwrap();
+    Python::with_gil(|py| PyBytes::new(py, &ipc).into())
+}
 
 #[pyfunction]
 fn read_bam(path: &str, region: Option<&str>) -> PyObject {
@@ -29,6 +45,8 @@ fn read_bcf(path: &str, region: Option<&str>) -> PyObject {
 #[pymodule]
 #[pyo3(name = "oxbow")]
 fn py_oxbow(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(read_fasta, m)?)?;
+    m.add_function(wrap_pyfunction!(read_fastq, m)?)?;
     m.add_function(wrap_pyfunction!(read_bam, m)?)?;
     m.add_function(wrap_pyfunction!(read_vcf, m)?)?;
     m.add_function(wrap_pyfunction!(read_bcf, m)?)?;
