e6e8b426606db6202c364a4ecb943c0e986a1786
Nezar Abdennur <nabdennur@gmail.com>
2023-07-29 10:12:19 -0400

    Update cargos

--- oxbow/src/bcf.rs
+++ oxbow/src/bcf.rs
@@ -5,13 +5,9 @@ use arrow::array::{
 use arrow::{
     datatypes::Int32Type, error::ArrowError, record_batch::RecordBatch,
 };
-use byteorder::{LittleEndian, ReadBytesExt};
 use noodles::core::Region;
-use noodles::bcf::header::StringMaps;
 use noodles::{bcf, bgzf, csi, vcf};
 use std::sync::Arc;
-use std::{ffi::CStr, io};
-use std::io::{Read};
 
 use crate::batch_builder::{write_ipc, BatchBuilder};
 use crate::vpos;
@@ -19,85 +15,22 @@ use crate::vpos;
 type BufferedReader = std::io::BufReader<std::fs::File>;
 
 
-fn read_u8<R>(reader: &mut R) -> io::Result<u8>
-where
-    R: Read,
-{
-    let mut buf = [0; 1];
-    reader.read_exact(&mut buf)?;
-    Ok(buf[0])
-}
-
-fn read_raw_header<R>(reader: &mut R) -> io::Result<String>
-where
-    R: Read,
-{
-    let l_text = reader.read_u32::<LittleEndian>().and_then(|n| {
-        usize::try_from(n).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
-    })?;
-
-    let mut buf = vec![0; l_text];
-    reader.read_exact(&mut buf)?;
-
-    CStr::from_bytes_with_nul(&buf)
-        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
-        .and_then(|c_header| {
-            c_header
-                .to_str()
-                .map(|s| s.into())
-                .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
-        })
-}
-
-fn read_bcf_header<R>(reader: &mut R) -> io::Result<(vcf::Header, bcf::header::StringMaps)>
-where
-    R: Read,
-{
-    // read the MAGIC number
-    let mut buf = [0; 3];
-    reader.read_exact(&mut buf)?;
-
-    let mut reader = reader;
-
-    // read the version
-    read_u8(&mut reader)?;
-    read_u8(&mut reader)?;
-
-    // read the raw header as a string
-    let s = read_raw_header(&mut reader)?;
-
-    // parse the VCF header and the string maps
-    let header: vcf::Header = s.parse().map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
-    let string_maps: bcf::header::StringMaps = s.parse().map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
-
-    Ok((header, string_maps))
-}
-
-
 /// A BCF reader.
 pub struct BcfReader {
     reader: bcf::Reader<bgzf::Reader<BufferedReader>>,
-    reader2: bcf::Reader<bgzf::Reader<std::fs::File>>,
     header: vcf::Header,
-    string_maps: StringMaps,
     index: csi::Index,
 }
 
 impl BcfReader {
     /// Creates a BCF Reader.
     pub fn new(path: &str) -> std::io::Result<Self> {
-        let file = std::fs::File::open(path)?;
         let index = csi::read(format!("{}.csi", path))?;
-        let bufreader = std::io::BufReader::with_capacity(1024 * 1024, file);
-        let reader = bcf::Reader::new(bufreader);
-
-        let mut bgzf_reader = std::fs::File::open(path).map(bgzf::Reader::new)?;
-        let (header, string_maps) = read_bcf_header(&mut bgzf_reader)?;
-
-        let mut reader2 = std::fs::File::open(path).map(bcf::Reader::new).unwrap();
-        let header = reader2.read_header().unwrap();
-
-        Ok(Self { reader, reader2, header, string_maps, index })
+        let file = std::fs::File::open(path)?;
+        let buf_file = std::io::BufReader::with_capacity(1024 * 1024, file);
+        let mut reader = bcf::Reader::new(buf_file);
+        let header = reader.read_header()?;
+        Ok(Self { reader, header, index })
     }
 
     /// Returns the records in the given region as Apache Arrow IPC.
@@ -114,7 +47,6 @@ impl BcfReader {
     /// ```
     pub fn records_to_ipc(&mut self, region: Option<&str>) -> Result<Vec<u8>, ArrowError> {
         let batch_builder = BcfBatchBuilder::new(1024, &self.header)?;
-        // let string_maps = StringMaps::try_from(&self.header).unwrap();
         if let Some(region) = region {
             let region: Region = region.parse().unwrap();
             let query = self
@@ -132,7 +64,7 @@ impl BcfReader {
         let vpos_lo = bgzf::VirtualPosition::try_from(pos_lo).unwrap();
         let vpos_hi = bgzf::VirtualPosition::try_from(pos_hi).unwrap();
         let batch_builder = BcfBatchBuilder::new(1024, &self.header)?;
-        let records = vpos::BcfRecords::new(&mut self.reader2, &self.header, vpos_lo, vpos_hi).map(|r| r.unwrap());
+        let records = vpos::BcfRecords::new(&mut self.reader, &self.header, vpos_lo, vpos_hi).map(|r| r.unwrap());
         write_ipc(records, batch_builder)
     }
 }
@@ -147,7 +79,6 @@ struct BcfBatchBuilder {
     filter: GenericStringBuilder<i32>,
     info: GenericStringBuilder<i32>,
     format: GenericStringBuilder<i32>,
-    header: vcf::Header,
 }
 
 impl BcfBatchBuilder {
@@ -172,7 +103,6 @@ impl BcfBatchBuilder {
             filter: GenericStringBuilder::<i32>::new(),
             info: GenericStringBuilder::<i32>::new(),
             format: GenericStringBuilder::<i32>::new(),
-            header: header.clone(),
         })
     }
 }
