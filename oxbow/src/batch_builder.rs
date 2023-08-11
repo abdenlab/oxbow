fc8758f7569f40affc6d186322941d70cfebf9d9
Jack <jack.huey@umassmed.edu>
2023-08-11 16:18:18 -0400

    Fix write_ipc maybe

--- oxbow/src/batch_builder.rs
+++ oxbow/src/batch_builder.rs
@@ -10,7 +10,7 @@ pub trait BatchBuilder {
 
 pub fn write_ipc<T>(
     records: impl Iterator<Item = T>,
-    mut batch_builder: impl for<'a> BatchBuilder<Record<'a> = T>,
+    mut batch_builder: impl for<'a> BatchBuilder<Record<'a> = &'a T>,
 ) -> Result<Vec<u8>, ArrowError> {
     records.for_each(|record| batch_builder.push(&record));
     let batch = batch_builder.finish()?;
