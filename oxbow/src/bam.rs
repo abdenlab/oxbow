6c9ee8580c3916847813a5359e9dd3039e58b111
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2024-11-29 21:33:52 -0800

    Use BufReader for reading BAM files with file-like objects

--- oxbow/src/bam.rs
+++ oxbow/src/bam.rs
@@ -17,12 +17,14 @@ use noodles::{bam, bgzf, csi, sam};
 
 use crate::batch_builder::{write_ipc_err, BatchBuilder};
 
-pub fn index_from_reader<R>(mut read: R) -> io::Result<csi::Index>
+pub fn index_from_reader<R>(read: R) -> io::Result<csi::Index>
 where
     R: Read + Seek,
 {
     // Unlike .tbi and .csi, .bai is not bgzf-compressed
     // so we read off the magic directly.
+    let mut buf_read = std::io::BufReader::with_capacity(1024 * 1024, file);
+
     let mut magic = [0; 4];
     read.read_exact(&mut magic)?;
     read.seek(io::SeekFrom::Start(0))?;
@@ -71,10 +73,11 @@ impl BamReader<BufReader<File>> {
     }
 }
 
-impl<R: Read + Seek> BamReader<R> {
+impl<R: Read + Seek> BamReader<BufReader<R>> {
     /// Creates a BAM reader.
     pub fn new(read: R, index: csi::Index) -> std::io::Result<Self> {
-        let mut reader = bam::Reader::new(read);
+        let buf_read = std::io::BufReader::with_capacity(1024 * 1024, read);
+        let mut reader = bam::Reader::new(buf_read);
         let header = reader.read_header()?;
         Ok(Self {
             reader,
