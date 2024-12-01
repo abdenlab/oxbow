d99326b095dde193e48f3a91ba622e263a733db1
Trevor Manz <trevor.j.manz@gmail.com>
2024-12-01 16:24:34 +0000

    v0.3.2 (#71)

--- r-oxbow/R/extendr-wrappers.R
+++ r-oxbow/R/extendr-wrappers.R
@@ -11,17 +11,37 @@
 #' @useDynLib oxbow, .registration = TRUE
 NULL
 
+#' Return Arrow IPC format from a FASTA file.
+#' @export
+read_fasta <- function(path, region) .Call(wrap__read_fasta, path, region)
+
+#' Return Arrow IPC format from a FASTQ file.
+#' @export
+read_fastq <- function(path) .Call(wrap__read_fastq, path)
+
 #' Return Arrow IPC format from a BAM file.
 #' @export
 read_bam <- function(path, region) .Call(wrap__read_bam, path, region)
+
+#' Return Arrow IPC format from a BAM file.
+#' @export
+read_bam_vpos <- function(path, cpos_lo, upos_lo, cpos_hi, upos_hi) .Call(wrap__read_bam_vpos, path, cpos_lo, upos_lo, cpos_hi, upos_hi)
 
 #' Return Arrow IPC format from a VCF file.
 #' @export
 read_vcf <- function(path, region) .Call(wrap__read_vcf, path, region)
 
+#' Return Arrow IPC format from a VCF file.
+#' @export
+read_vcf_vpos <- function(path, cpos_lo, upos_lo, cpos_hi, upos_hi) .Call(wrap__read_vcf_vpos, path, cpos_lo, upos_lo, cpos_hi, upos_hi)
+
 #' Return Arrow IPC format from a BCF file.
 #' @export
 read_bcf <- function(path, region) .Call(wrap__read_bcf, path, region)
+
+#' Return Arrow IPC format from a BCF file.
+#' @export
+read_bcf_vpos <- function(path, cpos_lo, upos_lo, cpos_hi, upos_hi) .Call(wrap__read_bcf_vpos, path, cpos_lo, upos_lo, cpos_hi, upos_hi)
 
 
 # nolint end
