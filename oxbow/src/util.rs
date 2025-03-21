3288b4c1391ad38d68201234fb760020b6acead9
Nezar Abdennur <nabdennur@gmail.com>
2025-03-20 20:23:20 -0400

    Run cargo fmt

--- oxbow/src/util.rs
+++ oxbow/src/util.rs
@@ -5,11 +5,8 @@ use arrow::ipc::writer::FileWriter;
 pub mod index;
 pub mod query;
 
-
 /// Serializes a sequence of Arrow record batches into Arrow IPC bytes.
-pub fn batches_to_ipc(
-    batches: impl RecordBatchReader,
-) -> Result<Vec<u8>, ArrowError> {
+pub fn batches_to_ipc(batches: impl RecordBatchReader) -> Result<Vec<u8>, ArrowError> {
     let schema = batches.schema();
     let mut writer = FileWriter::try_new(Vec::new(), &schema)?;
     for batch in batches {
