358c1e1f757184ccd610de75c70d2717c2e1a751
Nezar Abdennur <nabdennur@gmail.com>
2025-03-22 15:02:54 -0400

    Add tests for fields, batch builder, scanners

--- oxbow/src/sequence/format/fasta.rs
+++ oxbow/src/sequence/format/fasta.rs
@@ -96,3 +96,76 @@ impl Scanner {
         Ok(batch_iter)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use noodles::fasta::fai;
+    use std::io::BufReader;
+
+    #[test]
+    fn test_scanner_default() {
+        let scanner = Scanner::default();
+        assert_eq!(
+            scanner.field_names(),
+            FASTA_DEFAULT_FIELD_NAMES
+                .iter()
+                .map(|&s| s.to_string())
+                .collect::<Vec<_>>()
+        );
+    }
+
+    #[test]
+    fn test_scanner_schema() {
+        let scanner = Scanner::new();
+        let schema = scanner.schema(None).unwrap();
+        assert_eq!(schema.fields().len(), FASTA_DEFAULT_FIELD_NAMES.len());
+        let schema = scanner
+            .schema(Some(vec!["name".to_string(), "sequence".to_string()]))
+            .unwrap();
+        assert_eq!(schema.fields().len(), 2);
+    }
+
+    #[test]
+    fn test_scanner_scan() {
+        let data = b">seq1\nACGTACGTACGT\n>seq2\nTGCATGCATGCA\n>seq3\nGATTACAGATTACA\n";
+        let file = std::io::Cursor::new(data);
+        let reader = BufReader::new(file);
+        let fmt_reader = noodles::fasta::io::Reader::new(reader);
+
+        let scanner = Scanner::new();
+        let mut batch_iter = scanner.scan(fmt_reader, None, Some(2), Some(10)).unwrap();
+
+        let batch = batch_iter.next().unwrap().unwrap();
+        assert_eq!(batch.num_rows(), 2);
+        let batch = batch_iter.next().unwrap().unwrap();
+        assert_eq!(batch.num_rows(), 1);
+        assert!(batch_iter.next().is_none());
+    }
+
+    #[test]
+    fn test_scanner_scan_query() {
+        let data = b">seq1\nACGTACGTACGT\n>seq2\nTGCATGCATGCA\n>seq3\nGATTACAGATTACA\n";
+        let file = std::io::Cursor::new(data);
+        let reader = BufReader::new(file);
+        let fmt_reader = noodles::fasta::io::Reader::new(reader);
+        let index = fai::Index::from(vec![
+            fai::Record::new("seq1", 12, 0, 13, 13),
+            fai::Record::new("seq2", 12, 12, 13, 13),
+            fai::Record::new("seq3", 12, 24, 13, 13),
+        ]);
+
+        let scanner = Scanner::new();
+        let regions = vec!["seq1:1-4", "seq2:1-4", "seq3:1-4"];
+        let regions: Vec<Region> = regions.iter().map(|s| s.parse().unwrap()).collect();
+        let mut batch_iter = scanner
+            .scan_query(fmt_reader, index, regions, None, Some(2))
+            .unwrap();
+
+        let batch = batch_iter.next().unwrap().unwrap();
+        assert_eq!(batch.num_rows(), 2);
+        let batch = batch_iter.next().unwrap().unwrap();
+        assert_eq!(batch.num_rows(), 1);
+        assert!(batch_iter.next().is_none());
+    }
+}
