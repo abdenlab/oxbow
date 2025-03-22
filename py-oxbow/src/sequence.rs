b3e043aa1e0b7d8501e5217c9c391977f71e8c67
Nezar Abdennur <nabdennur@gmail.com>
2025-03-22 08:31:16 -0400

    Upgrade pyo3 and other deps and migrate pyo3 code

--- py-oxbow/src/sequence.rs
+++ py-oxbow/src/sequence.rs
@@ -18,14 +18,13 @@ use oxbow::util::batches_to_ipc;
 ///
 /// Parameters
 /// ----------
-/// obj : str or file-like
+/// src : str or file-like
 ///     The path to the FASTQ file or a file-like object.
 /// compressed : bool, optional [default: False]
 ///     Whether the source is GZIP-compressed. If None, it is assumed to be
 ///     uncompressed.
 #[pyclass]
 pub struct PyFastqScanner {
-    _obj: PyObject,
     reader: Reader,
     scanner: FastqScanner,
     compressed: bool,
@@ -34,12 +33,11 @@ pub struct PyFastqScanner {
 #[pymethods]
 impl PyFastqScanner {
     #[new]
-    fn new(py: Python, obj: PyObject, compressed: Option<bool>) -> PyResult<Self> {
-        let compressed = compressed.unwrap_or(false);
-        let reader = pyobject_to_bufreader(py, obj.clone(), false)?;
+    #[pyo3(signature = (src, compressed=false))]
+    fn new(py: Python, src: PyObject, compressed: bool) -> PyResult<Self> {
+        let reader = pyobject_to_bufreader(py, src, false)?;
         let scanner = FastqScanner::new();
         Ok(Self {
-            _obj: obj,
             reader,
             scanner,
             compressed,
@@ -61,9 +59,10 @@ impl PyFastqScanner {
     /// Returns
     /// -------
     /// arro3 Schema (pycapsule)
-    fn schema(&self, py: Python, fields: Option<Vec<String>>) -> PyResult<PyObject> {
+    #[pyo3(signature = (fields=None))]
+    fn schema(&self, fields: Option<Vec<String>>) -> PyResult<PySchema> {
         let schema = self.scanner.schema(fields)?;
-        Ok(PySchema::new(Arc::new(schema)).into_py(py))
+        Ok(PySchema::new(Arc::new(schema)))
     }
 
     /// Scan the source as record batches.
@@ -82,6 +81,7 @@ impl PyFastqScanner {
     /// -------
     /// arro3 RecordBatchReader (pycapsule)
     ///     An iterator yielding Arrow record batches.
+    #[pyo3(signature = (fields=None, batch_size=1024, limit=None))]
     fn scan(
         &mut self,
         fields: Option<Vec<String>>,
@@ -113,14 +113,14 @@ impl PyFastqScanner {
 ///
 /// Parameters
 /// ----------
-/// obj : str or file-like
+/// src : str or file-like
 ///     The path to the FASTA file or a file-like object.
 /// compressed : bool, optional [default: False]
 ///     Whether the source is BGZF-compressed. If None, it is assumed to be
 ///     uncompressed.
 #[pyclass]
 pub struct PyFastaScanner {
-    obj: PyObject,
+    src: PyObject,
     reader: Reader,
     scanner: FastaScanner,
     compressed: bool,
@@ -129,12 +129,12 @@ pub struct PyFastaScanner {
 #[pymethods]
 impl PyFastaScanner {
     #[new]
-    fn new(py: Python, obj: PyObject, compressed: Option<bool>) -> PyResult<Self> {
-        let compressed = compressed.unwrap_or(false);
-        let reader = pyobject_to_bufreader(py, obj.clone(), false)?;
+    #[pyo3(signature = (src, compressed=false))]
+    fn new(py: Python, src: PyObject, compressed: bool) -> PyResult<Self> {
+        let reader = pyobject_to_bufreader(py, src.clone_ref(py), false)?;
         let scanner = FastaScanner::new();
         Ok(Self {
-            obj,
+            src,
             reader,
             scanner,
             compressed,
@@ -156,9 +156,10 @@ impl PyFastaScanner {
     /// Returns
     /// -------
     /// arro3 Schema (pycapsule)
-    fn schema(&self, py: Python, fields: Option<Vec<String>>) -> PyResult<PyObject> {
+    #[pyo3(signature = (fields=None))]
+    fn schema(&self, fields: Option<Vec<String>>) -> PyResult<PySchema> {
         let schema = self.scanner.schema(fields)?;
-        Ok(PySchema::new(Arc::new(schema)).into_py(py))
+        Ok(PySchema::new(Arc::new(schema)))
     }
 
     /// Scan the source as record batches.
@@ -182,6 +183,7 @@ impl PyFastaScanner {
     /// -----
     /// Since reference sequences are often large, the default batch size is
     /// set to 1.
+    #[pyo3(signature = (fields=None, batch_size=1, limit=None))]
     fn scan(
         &mut self,
         fields: Option<Vec<String>>,
@@ -229,6 +231,7 @@ impl PyFastaScanner {
     /// If the source is BGZF-compressed, an additional GZI index is also
     /// required. The GZI index is used to translate uncompressed positions
     /// (from the FAI index) into compressed positions in the BGZF file.
+    #[pyo3(signature = (regions, index=None, gzi=None, fields=None, batch_size=1024))]
     fn scan_query(
         &mut self,
         py: Python,
@@ -239,7 +242,7 @@ impl PyFastaScanner {
         batch_size: Option<usize>,
     ) -> PyResult<PyRecordBatchReader> {
         // Load FAI index.
-        let index = resolve_faidx(py, self.obj.clone(), index)?;
+        let index = resolve_faidx(py, self.src.clone_ref(py), index)?;
         let reader = self.reader.clone();
 
         // Parse the genomic ranges.
@@ -254,7 +257,7 @@ impl PyFastaScanner {
         if self.compressed {
             // A BGZF IndexedReader seeks within a compressed file using uncompressed positions.
             let gzi_source = match gzi {
-                Some(gzi) => pyobject_to_bufreader(py, gzi.clone(), false)?,
+                Some(gzi) => pyobject_to_bufreader(py, gzi.clone_ref(py), false)?,
                 None => {
                     return Err(PyErr::new::<PyValueError, _>(
                         "GZI index required to query BGZF-encoded FASTA.",
@@ -297,16 +300,17 @@ impl PyFastaScanner {
 /// bytes
 ///     Arrow IPC
 #[pyfunction]
+#[pyo3(signature = (src, fields=None, compressed=false))]
 pub fn read_fastq(
     py: Python,
     src: PyObject,
     fields: Option<Vec<String>>,
-    compressed: Option<bool>,
+    compressed: bool,
 ) -> PyResult<Vec<u8>> {
     let reader = pyobject_to_bufreader(py, src, false)?;
     let scanner = FastqScanner::new();
 
-    let ipc = if compressed.unwrap_or(false) {
+    let ipc = if compressed {
         let gz_reader = std::io::BufReader::new(MultiGzDecoder::new(reader));
         let fmt_reader = noodles::fastq::io::Reader::new(gz_reader);
         let batches = scanner.scan(fmt_reader, fields, None, None)?;
@@ -345,6 +349,7 @@ pub fn read_fastq(
 /// bytes
 ///     Arrow IPC
 #[pyfunction]
+#[pyo3(signature = (src, regions=None, index=None, gzi=None, fields=None, compressed=false))]
 pub fn read_fasta(
     py: Python,
     src: PyObject,
@@ -352,10 +357,9 @@ pub fn read_fasta(
     index: Option<PyObject>,
     gzi: Option<PyObject>,
     fields: Option<Vec<String>>,
-    compressed: Option<bool>,
+    compressed: bool,
 ) -> PyResult<Vec<u8>> {
-    let compressed = compressed.unwrap_or(false);
-    let reader = pyobject_to_bufreader(py, src.clone(), compressed)?;
+    let reader = pyobject_to_bufreader(py, src.clone_ref(py), compressed)?;
     let scanner = FastaScanner::new();
 
     let ipc = if let Some(regions) = regions {
@@ -369,7 +373,7 @@ pub fn read_fasta(
             .collect::<Result<Vec<_>, _>>()?;
         if compressed {
             let gzi_source = match gzi {
-                Some(gzi) => pyobject_to_bufreader(py, gzi.clone(), false)?,
+                Some(gzi) => pyobject_to_bufreader(py, gzi.clone_ref(py), false)?,
                 None => {
                     return Err(PyErr::new::<PyValueError, _>(
                         "GZI index required to query BGZF-encoded FASTA.",
